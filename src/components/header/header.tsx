11466b9d794eceac1329203e5b6d2abc98fa234b
OlegJun <olegpotsah290@gmail.com>
2022-11-30 22:47:47 +0500

    has been created headers component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                Bite my wallet
+            </div>
+        </div>
+    );
+};
+
+export default Header;
